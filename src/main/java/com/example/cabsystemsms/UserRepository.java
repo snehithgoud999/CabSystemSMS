ff73f28c4f7637deceb58205a9905b161ac27a31
snehithgoud999 <snehith667@gmail.com>
2026-03-06 10:45:32 +0530

    CabSystemSMS  project

---
+++ src/main/java/com/example/cabsystemsms/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.cabsystemsms;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface UserRepository extends JpaRepository<Users,Integer>
+{
+   Users findByEmail(String email);
+}
